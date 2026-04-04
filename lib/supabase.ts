287747e2c6ad01df2a9690348beb6ad00daf9618
Yan Carlos <urbanoyaan@gmail.com>
2026-04-03 23:46:39 -0500

    feat: fix bugs and improving experience

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,11 @@
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { createClient } from '@supabase/supabase-js'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
 
-const supabaseUrl = "https://xweqqodptdkqqjpjgyer.supabase.co"
-const supabasePublishableKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh3ZXFxb2RwdGRrcXFqcGpneWVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3NTc3OTcsImV4cCI6MjA3NDMzMzc5N30.JGkfgj8IbCfiraxUKf7JBcOdaEHvr1SXOXSTr21j5uE"
+dotenv.config();
+
+const supabaseUrl = process.env.SUBABASE_URL!;
+const supabasePublishableKey = process.env.SUBABASE_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
     auth: {
