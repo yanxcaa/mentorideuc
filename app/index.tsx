62b460e48c83955ddfe472ccb2bd21d551adeae5
Yan Carlos <urbanoyaan@gmail.com>
2026-04-07 23:11:38 -0500

    Debuging log

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { View } from 'react-native';
 import AuthController from '@/src/components/AuthController';
 
+// New debug
 export default function App() {
     return (
         <View className="flex-1">
